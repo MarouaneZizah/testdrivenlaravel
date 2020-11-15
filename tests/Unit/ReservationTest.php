0becb0ab4dc7008ae8229eddaaad447863429286
Marouane Zizah <16108136+MarouaneZizah@users.noreply.github.com>
2020-11-15 15:07:34 +0100

    Reservation Test

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -3,22 +3,21 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use App\Models\Concert;
 use App\Models\Reservation;
 use App\Exceptions\NotEnoughTicketsException;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ReservationTest extends TestCase {
-    use DatabaseMigrations;
-
     /** @test*/
     public function can_calculate_reservation_cost() {
-        $concert = Concert::factory()->create(['price' => 1200])->addTickets(5);
-
-        $tickets = $concert->findTickets(2);
+        $tickets = collect([
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+        ]);
 
         $reservation = new Reservation($tickets);
 
-        $this->assertEquals(2400, $reservation->totalCost());
+        $this->assertEquals(3600, $reservation->totalCost());
     }
 }
